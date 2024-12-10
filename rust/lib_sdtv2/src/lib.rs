7f6ddb5db15e3ea43479028d087b837579db63c8
Michael Abel <info@abel-ikt.de>
2024-12-10 20:02:44 +0100

    Add test for sdt_set_sdsink_parameters

--- rust/lib_sdtv2/src/lib.rs
+++ rust/lib_sdtv2/src/lib.rs
@@ -9,19 +9,6 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-/*
-unsafe extern "C" {
-    pub fn sdt_get_validator(
-        type_: sdt_bus_type_t,
-        sid1: u32,
-        sid2: u32,
-        sid2red: u8,
-        version: u16,
-        p_hnd: *mut sdt_handle_t,
-    ) -> sdt_result_t;
-}
-
- * */
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,11 +34,55 @@ mod tests {
         let sid2: u32 = 41;
         let sid2red: u8 = 8;
         let version: u16 = 2;
-        let mut sdt_handle: i32 = 0;
+        let mut sdt_handle: i32 = 88;
 
         let p_handle: *mut sdt_handle_t = &mut sdt_handle as *mut i32;
         let result = unsafe { sdt_get_validator(bus_type, sid1, sid2, sid2red, version, p_handle) };
 
         println!("Get Validator: {:?}", result);
+        assert_ne!(sdt_handle, 88);
+        assert_eq!(result, sdt_result_t_SDT_OK);
+    }
+
+    /*
+    unsafe extern "C" {
+        pub fn sdt_set_sdsink_parameters(
+            handle: sdt_handle_t,
+            rx_period: u16,
+            tx_period: u16,
+            n_rxsafe: u8,
+            n_guard: u16,
+            cmthr: u32,
+            lmi_max: u16,
+        ) -> sdt_result_t;
+    }
+    */
+
+    #[test]
+    fn test_sdt_set_sdsink_parameters() {
+        let bus_type: sdt_bus_type_t = sdt_bus_type_t_SDT_IPT;
+        let sid1: u32 = 42;
+        let sid2: u32 = 43;
+        let sid2red: u8 = 8;
+        let version: u16 = 2;
+        let mut sdt_handle: i32 = 88;
+
+        let p_handle: *mut sdt_handle_t = &mut sdt_handle as *mut i32;
+        let result = unsafe { sdt_get_validator(bus_type, sid1, sid2, sid2red, version, p_handle) };
+
+        assert_ne!(sdt_handle, 88);
+        assert_eq!(result, sdt_result_t_SDT_OK);
+
+        let result = unsafe {
+            sdt_set_sdsink_parameters(
+                sdt_handle, 100,  /*ms Trx*/
+                120,  /*ms Ttx*/
+                5,    /*n_rxsafe*/
+                2,    /*n_guard*/
+                1000, /*cmthr*/
+                200,  /*lmi_max*/
+            )
+        };
+        assert_eq!(result, sdt_result_t_SDT_OK);
     }
 }
