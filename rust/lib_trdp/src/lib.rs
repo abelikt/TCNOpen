0300d8ad10a09d87c661b485ddc92e4d3aa1e70f
Michael Abel <info@abel-ikt.de>
2024-12-14 20:22:32 +0100

    Refactor

--- rust/lib_trdp/src/lib.rs
+++ rust/lib_trdp/src/lib.rs
@@ -27,32 +27,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
-    }
-
-    //     pub fn tlc_init(
-    //         pPrintDebugString: TRDP_PRINT_DBG_T,
-    //         pRefCon: *mut ::std::os::raw::c_void,
-    //         pMemConfig: *const TRDP_MEM_CONFIG_T,
-    //     ) -> TRDP_ERR_T;
-    //
-    //     pub static mut gPDebugFunction: VOS_PRINT_DBG_T;
-    //
-    // pub struct TRDP_MEM_CONFIG_T {
-    //     pub p: *mut UINT8,
-    //     pub size: UINT32,
-    //     pub prealloc: [UINT32; 15usize],
-    // }
-    //
-    //
-    #[test]
     fn test_tlc_init() {
         let err: TRDP_ERR_T;
-        //let pPrintDebugString= ptr::null() as std::os::raw::c_void;
         let pRefCon: *mut raw::c_void = ptr::null_mut();
-
-
+        
         // Todo direct assignment fails, so we pipe through callback
         // = note: expected enum `Option<unsafe extern "C" fn(_, _, _, _, _, _)>`
         // found enum `Option<unsafe extern "C" fn(_, _, _, _, _, _) {debug_callback}>`
@@ -60,12 +38,12 @@ mod tests {
         let callback : unsafe extern "C" fn(_, _, _, _, _, _)  = debug_callback;
         let debug = Some( callback );
 
-        //     TRDP_MEM_CONFIG_T       dynamicConfig   = {NULL, RESERVED_MEMORY, {0}};
         let memConfig = TRDP_MEM_CONFIG_T {
             p: ptr::null_mut() as *mut UINT8,
             size: 160000,
             prealloc: [0; 15],
         };
+
         let pMemConfig = &memConfig as *const TRDP_MEM_CONFIG_T;
 
         let err = unsafe { tlc_init(debug, pRefCon, pMemConfig) };
